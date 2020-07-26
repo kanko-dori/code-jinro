ba1e02eea99fa38a30bbe61f6644eb6671f62f33
ssssota <tommysouta@gmail.com>
2020-07-26 12:01:51 +0900

    :sparkles: Add routing

---
+++ hosting/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+interface Props {}
+interface State {}
+class Home extends React.Component<Props, State> {
+  render() {
+    return (
+      <div>
+        HOME
+      </div>
+    );
+  }
+};
+export default Home;
