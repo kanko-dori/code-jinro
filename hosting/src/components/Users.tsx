3c49d9b0d171365bb1dbb666a5a53b9a75b34743
ssssota <tommysouta@gmail.com>
2020-07-27 00:20:08 +0900

    :lipstick: Add vote button for userlist

--- hosting/src/components/Users.tsx
+++ hosting/src/components/Users.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import {
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Button
+} from "@material-ui/core";
+
 import classes from "./Users.module.css";
 
 interface Props {}
 interface State {
   users: User[];
+  voteUser?: string;
 }
 
 class Users extends React.Component<Props, State> {
@@ -12,21 +22,37 @@ class Users extends React.Component<Props, State> {
     super(props);
 
     this.state = {
-      users: [ new User("Tom") ]
+      users: [ new User("Tom") ],
     }
   }
   render() {
     return (
       <div className={classes.container}>
-        <ul className={classes.userlist}>
-          {
-            this.state.users.map(user =>
-              <li key={user.name}>{user.name}</li>
-            )
-          }
-        </ul>
+        <form onSubmit={this.onVoteUser}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Vote writing user</FormLabel>
+            <RadioGroup value={this.state.voteUser} onChange={this.onRadioChange}>
+              {
+                this.state.users.map(user =>
+                  <FormControlLabel key={user.name} value={user.name} control={<Radio />} label={user.name} />
+                )
+              }
+              
+            </RadioGroup>
+          </FormControl>
+          <p>
+            <Button type="submit" variant="contained">Vote</Button>
+          </p>
+        </form>
       </div>
     );
+  }
+
+  onRadioChange(event: React.ChangeEvent<HTMLInputElement>, value: string) {
+    console.log(event);
+  }
+  onVoteUser(event: React.FormEvent<HTMLFormElement>) {
+    console.log(event);
   }
 };
 export default Users;
