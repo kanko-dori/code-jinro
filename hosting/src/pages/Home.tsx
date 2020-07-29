ac86ee9ae1f2c8037c8df9556d16f247609adf4c
ssssota <tommysouta@gmail.com>
2020-07-29 21:08:41 +0900

    :rotating_light: Fix class to function

--- hosting/src/pages/Home.tsx
+++ hosting/src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-interface Props {}
-interface State {}
-class Home extends React.Component<Props, State> {
-  render() {
-    return (
-      <div>
-        HOME
-      </div>
-    );
-  }
+function Home(): JSX.Element {
+  return (
+    <div>
+      HOME
+    </div>
+  );
 }
 export default Home;
