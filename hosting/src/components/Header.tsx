1269210675410e5bbc28b182678780552779dfd6
ssssota <tommysouta@gmail.com>
2020-08-01 01:36:08 +0900

    :sparkles: Add share button

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -4,27 +4,82 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  MenuItem,
   IconButton,
+  Snackbar,
 } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import { Share } from '@material-ui/icons';
 import Spacer from './Spacer';
+import { copyText } from '../utils/util';
 import classes from './Header.module.css';
 
-const Header: React.FC = () => (
-  <AppBar>
-    <Toolbar variant="dense">
-      <Typography variant="h6">
-        <span className={classes.title}>Code JINRO</span>
-      </Typography>
-      <Spacer />
-      <MenuItem>
-        <IconButton>
-          <Share />
-        </IconButton>
-      </MenuItem>
-    </Toolbar>
-  </AppBar>
-);
+interface State {
+  copySnackOpen: boolean;
+  snackSeverity: 'success' | 'error';
+  copyResultMsg: 'Copied URL!' | 'Failed copy.';
+}
+
+class Header extends React.Component<unknown, State> {
+  constructor(props: unknown) {
+    super(props);
+
+    this.onShareClick = this.onShareClick.bind(this);
+    this.onSnackClose = this.onSnackClose.bind(this);
+
+    this.state = {
+      copySnackOpen: false,
+      snackSeverity: 'success',
+      copyResultMsg: 'Copied URL!',
+    };
+  }
+
+  onShareClick(): void {
+    try {
+      copyText(window.location.origin + window.location.pathname);
+      this.setState({
+        copySnackOpen: true,
+        snackSeverity: 'success',
+      });
+    } catch (e) {
+      this.setState({
+        copySnackOpen: true,
+        snackSeverity: 'error',
+      });
+    }
+  }
+
+  onSnackClose(): void {
+    this.setState({
+      copySnackOpen: false,
+    });
+  }
+
+  render(): JSX.Element {
+    return (
+      <AppBar>
+        <Toolbar variant="dense">
+          <Typography variant="h6">
+            <span className={classes.title}>Code JINRO</span>
+          </Typography>
+          <Spacer />
+          <IconButton onClick={this.onShareClick}>
+            <Share />
+          </IconButton>
+        </Toolbar>
+
+        <Snackbar
+          open={this.state.copySnackOpen}
+          autoHideDuration={2000}
+          onClose={this.onSnackClose}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        >
+          <Alert onClose={this.onSnackClose} severity={this.state.snackSeverity}>
+            {this.state.copyResultMsg}
+          </Alert>
+        </Snackbar>
+      </AppBar>
+    );
+  }
+}
 
 export default Header;
