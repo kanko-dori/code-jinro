49ec636a41768d743d80623f3dcf4101342ab91c
watamoko <kei07161@yahoo.co.jp>
2020-08-03 21:13:21 +0900

    インデント揃えました

--- hosting/src/components/BugForm.tsx
+++ hosting/src/components/BugForm.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const BugForm: React.FC = () => {
-    return (
-        <div>
-            <h3>バグ報告</h3>
-            <textarea>メッセ欄？</textarea>
-            <button>送信</button>
-        </div>
-    );
+	return (
+		<div>
+			<h3>バグ報告</h3>
+			<textarea>メッセ欄？</textarea>
+			<button>送信</button>
+		</div>
+	);
 };
 
 export default BugForm;
--- hosting/src/components/Rule.tsx
+++ hosting/src/components/Rule.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Rule: React.FC = () => {
-    return (
-        <div>
-            <h3>Rules</h3>
-            <div>ここにルールを記載</div>
-        </div>
-    );
+	return (
+		<div>
+			<h3>Rules</h3>
+			<div>ここにルールを記載</div>
+		</div>
+	);
 };
 
 export default Rule;
