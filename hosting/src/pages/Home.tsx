7e2c68fb0b9f8110dfd4427d3d059fc4ed62161e
ssssota <tommysouta@gmail.com>
2020-08-03 18:11:59 +0900

    📍 Fix append toolbar under toolbar

--- hosting/src/pages/Home.tsx
+++ hosting/src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import { Toolbar } from '@material-ui/core';
+
 const Home: React.FC = () => (
-  <div>
-    HOME
-  </div>
+  <>
+    <Toolbar />
+    <div>contents</div>
+  </>
 );
 export default Home;
