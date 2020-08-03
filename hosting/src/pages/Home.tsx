69b5db61d5290ee763a2df464d63ba4da976a519
watamoko <kei07161@yahoo.co.jp>
2020-08-03 21:00:44 +0900

    GitHubリンクを追加

--- hosting/src/pages/Home.tsx
+++ hosting/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { Toolbar } from '@material-ui/core';
-import Rule from '../components/Rule'
+import Rule from '../components/Rule';
+import CreateRoom from '../components/CreateRoom';
+import BugForm from '../components/BugForm';
 
 class Home extends React.Component {
   render(): JSX.Element {
@@ -9,6 +11,9 @@ class Home extends React.Component {
       <div>
         <Toolbar />
         <Rule />
+        <CreateRoom />
+        <BugForm />
+        <a href="https://github.com/kanko-dori/code-jinro">GitHub</a>
       </div>
     )
   };
