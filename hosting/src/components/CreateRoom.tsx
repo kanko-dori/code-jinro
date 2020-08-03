9188e73afc21730b08b434b8168d1ffaa53e0310
watamoko <kei07161@yahoo.co.jp>
2020-08-03 21:13:21 +0900

    インデント揃えました

--- hosting/src/components/CreateRoom.tsx
+++ hosting/src/components/CreateRoom.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const CreateRoom: React.FC = () => {
-    return (
-        <div>
-            <h3>ゲーム開始</h3>
-            <button>CreateRoom</button>
-        </div>
-    );
+	return (
+		<div>
+			<h3>ゲーム開始</h3>
+			<button>CreateRoom</button>
+		</div>
+	);
 };
 
 export default CreateRoom;
