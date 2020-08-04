dc70da99181734f6f1c63aa37483ce0ed638bf17
watamoko <kei07161@yahoo.co.jp>
2020-08-04 09:38:23 +0900

    できるところまでMaterial-UI入れた

--- hosting/src/components/Rule.tsx
+++ hosting/src/components/Rule.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Rule: React.FC = () => {
-	return (
-		<div>
-			<h3>Rules</h3>
-			<div>ここにルールを記載</div>
-		</div>
-	);
-};
+const Rule: React.FC = () => (
+	<div>
+		<h3>Rules</h3>
+		<div>ここにルールを記載</div>
+	</div>
+);
 
 export default Rule;
--- hosting/src/pages/Home.tsx
+++ hosting/src/pages/Home.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
-import { Toolbar } from '@material-ui/core';
+import { Toolbar, Link } from '@material-ui/core';
 import Rule from '../components/Rule';
 import CreateRoom from '../components/CreateRoom';
 import BugForm from '../components/BugForm';
 
-class Home extends React.Component {
-  render(): JSX.Element {
-    return (
-      <div>
-        <Toolbar />
-        <Rule />
-        <CreateRoom />
-        <BugForm />
-        <a href="https://github.com/kanko-dori/code-jinro">GitHub</a>
-      </div>
-    )
-  };
-}
+const Home: React.FC = () => (
+  <div>
+    <Toolbar />
+    <Rule />
+    <CreateRoom />
+    <BugForm />
+    <Link href="https://github.com/kanko-dori/code-jinro">GitHub</Link>
+  </div>
+);
 
 export default Home;
