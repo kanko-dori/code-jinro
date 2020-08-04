c068a75ae1b9dfad9eaa0d80dcf9d7f18dc71b23
watamoko <kei07161@yahoo.co.jp>
2020-08-04 23:37:00 +0900

    今度こそインデント揃えました

--- hosting/src/components/Rule.tsx
+++ hosting/src/components/Rule.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Paper } from '@material-ui/core';
 
 const Rule: React.FC = () => (
-	<Paper>
-		<h3>Rules</h3>
-		<div>ここにルールを記載</div>
-	</Paper>
+  <Paper>
+    <h3>Rules</h3>
+    <div>ここにルールを記載</div>
+  </Paper>
 );
 
 export default Rule;
