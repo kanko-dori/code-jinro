c5c72213ba2aa13f281d8e75960a67770107b304
watamoko <kei07161@yahoo.co.jp>
2020-08-04 23:37:00 +0900

    今度こそインデント揃えました

--- hosting/src/components/CreateRoom.tsx
+++ hosting/src/components/CreateRoom.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Button, Paper } from '@material-ui/core';
 
 const CreateRoom: React.FC = () => (
-	<Paper>
-		<h3>ゲーム開始</h3>
-		<Button variant="contained">CreateRoom</Button>
-	</Paper>
+  <Paper>
+    <h3>ゲーム開始</h3>
+    <Button variant="contained">CreateRoom</Button>
+  </Paper>
 );
 
 export default CreateRoom;
--- hosting/src/components/ReportForm.tsx
+++ hosting/src/components/ReportForm.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import { TextField, Button, Card } from '@material-ui/core';
 
 const ReportForm: React.FC = () => (
-	<Card>
-		<form>
-			<h3>バグ報告</h3>
-			<TextField label="Message" variant="outlined" multiline />
-			<Button
-				variant="contained"
-				color="primary"
-				type="submit"
-			>
-				Send
-			</Button>
-		</form>
-	</Card>
+  <Card>
+    <form>
+      <h3>バグ報告</h3>
+      <TextField label="Message" variant="outlined" multiline />
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        Send
+      </Button>
+    </form>
+  </Card>
 );
 
 export default ReportForm;
