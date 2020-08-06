b41cf824de607a773a9e0e1c0532f3cd1fe4a95a
ssssota <tommysouta@gmail.com>
2020-08-06 10:08:33 +0900

    🧱Add notification component (snackbar wrapper)

---
+++ hosting/src/components/Notification.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Snackbar } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+
+type AlertType = 'error' | 'warning' | 'info' | 'success';
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  autoHideDuration?: number;
+  severnity: AlertType;
+  children: React.ReactNode;
+  anchorOrigin?: { horizontal: 'center' | 'left' | 'right', vertical: 'bottom' | 'top' },
+  color?: AlertType | undefined;
+  variant?: 'filled' | 'outlined' | 'standard';
+}
+
+const Notification: React.FC<Props> = (props: Props) => (
+  <Snackbar
+    open={props.open}
+    onClose={props.onClose}
+    autoHideDuration={props.autoHideDuration}
+    anchorOrigin={props.anchorOrigin}
+  >
+    <Alert
+      onClose={props.onClose}
+      severity={props.severnity}
+      color={props.color}
+      variant={props.variant}
+    >
+      {props.children}
+    </Alert>
+  </Snackbar>
+);
+
+Notification.defaultProps = {
+  autoHideDuration: 3000,
+  anchorOrigin: { vertical: 'bottom', horizontal: 'center' },
+  color: undefined,
+  variant: 'standard',
+};
+
+export default Notification;
