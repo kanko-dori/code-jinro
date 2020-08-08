61fcb7abb7f58cab0ce3ea144266e443c36501bc
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-08 11:05:49 +0900

    add generateRandomID

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+// Import Admin SDK
+// import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+// // Attach an asynchronous callback to read the data at our posts reference
+// ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
 // });
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const createNewRoom = functions.https.onRequest((request: functions.Request, response: functions.Response) => {
+    // Get a database reference to our posts
+    const uid = generateRandomID(10);
+    // reques
+    functions.logger.info(uid, { structuredData: true });
+    response.send(uid);
+});
+
+
+const generateRandomID = (length: number) => {
+    let result           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
