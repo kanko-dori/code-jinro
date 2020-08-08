66118eca83bd74a8d538700e6f312cd199d1a918
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-08 13:37:02 +0900

    :poop: add cors allow all access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,11 @@ import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 
 import * as express from 'express';
-// import * as cors from 'cors';
+import * as cors from 'cors';
 import * as handler from './handler'
 
 const app = express();
-// app.use(cors);
+app.use(cors({}));
 firebase.initializeApp();
 
 app.get('/', handler.ping);
