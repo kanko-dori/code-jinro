1ef2bc51e8f98f66409bbe2374ad83d164fa2aae
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-08 13:39:35 +0900

    :sparkles: Implementing page transitions

--- hosting/src/components/CreateRoom.tsx
+++ hosting/src/components/CreateRoom.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-
 import { Button, Paper, Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { Room } from '../types/types';
 
-const CreateRoom: React.FC = () => (
-  <Paper>
-    <Typography variant="h5">ゲーム開始</Typography>
-    <Button variant="contained">CreateRoom</Button>
-  </Paper>
-);
+type response = {
+  room: Room,
+  id: string
+}
+
+const CreateRoom: React.FC = () => {
+  const history = useHistory();
+
+  const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    fetch(`https://us-central1-code-jinro.cloudfunctions.net/api/room/${process.env.NODE_ENV}`, { method: 'PUT' })
+      .then((response) => response.json())
+      .then((data: response) => {
+        console.log(data);
+        history.push(`/room/${data.id}`);
+      }).catch((err) => {
+        console.log('fetch failed: ', err);
+      });
+  };
+
+  return (
+    <Paper>
+      <Typography variant="h5">ゲーム開始</Typography>
+      <Button variant="contained" onClick={handleClick}>CreateRoom</Button>
+    </Paper>
+  );
+};
 
 export default CreateRoom;
