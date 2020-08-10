661d635556124118756e59e62c730e994af1ea14
ssssota <tommysouta@gmail.com>
2020-08-10 13:02:58 +0900

    🖇 Add export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import * as firebase from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as handler from './handler'
+import * as onAuth from './auth';
 
 const app = express();
 app.use(cors({}));
@@ -17,3 +18,6 @@ app.post('/:stage/room', handler.createNewRoom);
 // // https://firebase.google.com/docs/functions/typescript
 //
 exports.api = functions.https.onRequest(app);
+
+exports.authCreate = functions.auth.user().onCreate(onAuth.createUserSecret);
+exports.authDelete = functions.auth.user().onDelete(onAuth.deleteUserSecret);
