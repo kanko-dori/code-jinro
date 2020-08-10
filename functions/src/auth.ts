a5c5b0cb2db6e07d07bb59ab00c72e0c69d0e75f
ssssota <tommysouta@gmail.com>
2020-08-10 21:18:04 +0900

    📝 Fix lint error

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -2,12 +2,12 @@ import * as firebase from 'firebase-admin';
 import { UserRecord } from 'firebase-functions/lib/providers/auth';
 import * as crypto from 'crypto';
 
-const generateRandomString = (len: number = 32) => crypto.randomBytes(len).toString('base64').substring(0, len);
+const generateRandomString = (len = 32) => crypto.randomBytes(len).toString('base64').substring(0, len);
 
-export const createUserSecret = (user: UserRecord) => {
+export const createUserSecret = (user: UserRecord): void => {
   firebase.database().ref(`secrets/${user.uid}`).set(generateRandomString());
 };
 
-export const deleteUserSecret = (user: UserRecord) => {
+export const deleteUserSecret = (user: UserRecord): void => {
   firebase.database().ref(`secrets/${user.uid}`).remove();
 };
