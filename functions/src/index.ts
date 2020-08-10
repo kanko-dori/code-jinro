2cf75393766bbfa5a8551366b89bd15bb41dfc68
ssssota <tommysouta@gmail.com>
2020-08-10 22:23:02 +0900

    📝 Fix endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,9 +10,9 @@ const app = express();
 app.use(cors({}));
 firebase.initializeApp();
 
-app.get('/', handler.ping);
+app.get('/api', handler.ping);
 
-app.post('/:stage/room', handler.createNewRoom);
+app.post('/api/:stage/room', handler.createNewRoom);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
