e0a7304834767b42609ed7f067cd8339e51e42ac
watamoko <kei07161@yahoo.co.jp>
2020-08-15 22:14:10 +0900

    add sendReport func

--- hosting/src/components/ReportForm.tsx
+++ hosting/src/components/ReportForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   TextField,
@@ -11,21 +11,45 @@ import {
 import Spacer from './Spacer';
 import classes from './ReportForm.module.css';
 
-const ReportForm: React.FC = () => (
-  <Card className={classes.container}>
-    <form>
-      <CardContent>
-        <Typography variant="h5">バグ報告</Typography>
-        <TextField label="Message" variant="outlined" multiline fullWidth rows={3} />
-      </CardContent>
-      <CardActions>
-        <Spacer />
-        <Button color="inherit" type="submit">
-          Send
+const ReportForm: React.FC = () => {
+  const [formText, setFormText] = useState<string>('');
+
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormText(e.target.value);
+  }
+
+  const sendReport = () => {
+    console.log(formText);
+    fetch(`/api/${process.env.NODE_ENV}/report`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        content: formText,
+      }),
+    }).then((res) => {
+      if(res.ok) return;
+      throw new Error(res.statusText);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  return (
+    <Card className={classes.container}>
+      <form>
+        <CardContent>
+          <Typography variant="h5">バグ報告</Typography>
+          <TextField label="Message" variant="outlined" multiline fullWidth rows={3} onChange={e => handleFormChange(e)} />
+        </CardContent>
+        <CardActions>
+          <Spacer />
+          <Button color="inherit" type="submit" onClick={() => sendReport()}>
+            Send
         </Button>
-      </CardActions>
-    </form>
-  </Card>
-);
+        </CardActions>
+      </form>
+    </Card>
+  );
+};
 
 export default ReportForm;
