a9ff029437923e61feba979c3567d817dbc11ab3
Sotaro Tommykawa <tommysouta@gmail.com>
2020-08-22 23:57:16 +0900

    ✨ Add room creater

--- functions/src/handler.ts
+++ functions/src/handler.ts
@@ -2,40 +2,41 @@ import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 
 import { Room } from './types/types';
-import { languages } from './utils/constants';
+import { languages, stages } from './utils/constants';
 
 export const ping = (request: functions.Request, response: functions.Response): void => {
   functions.logger.info('ping');
   response.sendStatus(200);
 };
 
-export const createNewRoom = (request: functions.Request, response: functions.Response):void => {
-  try {
-    const { stage } = request.params;
-    if (!['production', 'staging', 'development'].includes(stage)) {
-      response.status(400).send('Invalid Stage');
-      return;
-    }
+const getRoomsReference = (stage: string) => firebase.database().ref(`${stage}/rooms`);
+const getRoomReference = (stage: string, id: string) => getRoomsReference(stage).child(id);
 
-    const realtimeDB = firebase.database();
-    const roomsRef = realtimeDB.ref(`${stage}/rooms`);
-    const newRoomRef = roomsRef.push();
-    const newRoom :Room = {
-      currentRound: {
-        language: languages[0],
-        problemURL: '',
-        code: '',
-      },
-      users: {},
-      state: 'waiting',
-      history: [],
-    };
+const checkStage = (stage: string) => {
+  if ((stages as readonly string[]).includes(stage)) return 0;
+  throw new functions.https.HttpsError('invalid-argument', 'Invalid Stage');
+};
 
-    newRoomRef.set(newRoom).then((room) => {
-      console.log(room);
-      response.status(201).send({ id: newRoomRef.key });
-    });
-  } catch (err) {
-    response.sendStatus(500).send(err);
-  }
+export const createRoom = (data: any, context: functions.https.CallableContext) => {
+  checkStage(data.stage);
+
+  const roomsRef = getRoomsReference(data.stage);
+  const roomRef = roomsRef.push();
+  const room :Room = {
+    currentRound: {
+      language: languages[0],
+      problemURL: '',
+      code: '',
+    },
+    users: {},
+    state: 'waiting',
+    history: [],
+  };
+
+  return roomRef.set(room).then((room) => {
+    console.log(room);
+    return { roomId: roomRef.key };
+  }).catch((err: Error) => {
+    throw new functions.https.HttpsError('internal', err.message);
+  });
 };
